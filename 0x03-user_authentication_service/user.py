294ca9747af8c4b59038cd23ffdec4e0dd274498
Shaker Sharabi <shakersharabi100@gmail.com>
2024-11-21 06:55:58 +0200

    Add user.py file

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -4,15 +4,15 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
-Base = declartive_base()
+Base = declarative_base()
 
 
-class User(base):
+class User(Base):
     """User Model"""
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=true)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String, nullable=True)
-    reset_token = Column(String, nullable=True)
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
