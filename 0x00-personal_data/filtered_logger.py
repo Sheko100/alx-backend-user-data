ccde14b9c5410e47b2db09122013b489f3250736
Shaker Sharabi <shakersharabi100@gmail.com>
2025-01-26 08:32:57 +0200

    Add filtered_logger.py file

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,13 +2,15 @@
 """Module that obfuscates data
 """
 import re
+from typing import List
+import logging
 
 
-def filter_datum(fields: list[str], redaction: str,
-                 message: list[str], separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """obfuscates data
     """
-    message_chunks = re.split(separator, message)
+    message_chunks = re.split(separator, message)[:-1]
     chunks_count = len(message_chunks)
     new_message = ''
     for i in range(chunks_count):
@@ -16,7 +18,25 @@ def filter_datum(fields: list[str], redaction: str,
         for field in fields:
             if (re.search('^{}=.*'.format(field), chunk)):
                 chunk = re.sub('=.*', '={}'.format(redaction), chunk)
-
         new_message += '{};'.format(chunk)
-
     return new_message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Formats the log message"""
+        return filter_datum(self.fields, self.REDACTION,
+                            super(RedactingFormatter, self).format(record),
+                            self.SEPARATOR
+                            )
