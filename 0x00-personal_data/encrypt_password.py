3efe2213dac9ee586132e725004b607ca94ec1b1
Shaker Sharabi <shakersharabi100@gmail.com>
2025-01-27 04:31:04 +0200

    Add encrypt_password.py file

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -9,3 +9,9 @@ def hash_password(pwd: str) -> bytes:
     """
     hashed = bcrypt.hashpw(pwd.encode("utf-8"), bcrypt.gensalt())
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Validates the password hash
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
