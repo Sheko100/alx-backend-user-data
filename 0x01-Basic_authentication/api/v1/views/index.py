3034cb7455d9a64047bc897f03af74076b63139b
Shaker Sharabi <shakersharabi100@gmail.com>
2025-01-29 16:54:13 +0200

    add unauthorized error handler

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,10 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', strict_slashes=False)
+def unauthorized() -> str:
+    """ aborts with 401 error
+    """
+    abort(401)
