487af72bf5fb339bb5bc12bd1ffdca5148701211
Shaker Sharabi <shakersharabi100@gmail.com>
2025-01-31 06:22:23 +0200

    update return value

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -58,7 +58,7 @@ class BasicAuth(Auth):
 
         user = users[0]
 
-        if user.is_valid_password(user_pwd):
-            return user
+        if not user.is_valid_password(user_pwd):
+            return None
 
-        return None
+        return user
