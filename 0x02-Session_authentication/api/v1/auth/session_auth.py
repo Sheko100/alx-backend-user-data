b51ce2d3faef56227c5a60e1fb8c34e2bcfc7d7b
Shaker Sharabi <shakersharabi100@gmail.com>
2025-02-04 04:59:26 +0200

    Add user_id getting method

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -20,3 +20,14 @@ class SessionAuth(Auth):
         self.user_id_by_session_id[session_id] = user_id
 
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Gets user id by session id
+        """
+
+        if not session_id or not isinstance(session_id, str):
+            return None
+
+        user_id = self.user_id_by_session_id.get(session_id)
+
+        return user_id
