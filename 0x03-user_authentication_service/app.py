6dc7f11ef14142a742c17d03dfc8b57baa21309d
Shaker Sharabi <shakersharabi100@gmail.com>
2025-02-06 16:34:14 +0200

    Add app.py file

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Module that defines a flask app
 """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 auth = Auth()
@@ -27,6 +27,24 @@ def users():
         return jsonify({'email': email, 'message': 'user created'})
     except ValueError:
         return jsonify({'message': 'email already registered'}), 400
+
+
+@app.route('/sessions', methods=['POST'])
+def login():
+    """Logs in
+    """
+    email = request.form['email']
+    password = request.form['password']
+
+    if not auth.valid_login(email, password):
+        abort(401)
+
+    session_id = auth.create_session(email)
+    response = jsonify({'email': email, 'message': 'logged in'})
+
+    response.set_cookie('session_id', session_id)
+
+    return response
 
 
 if __name__ == "__main__":
