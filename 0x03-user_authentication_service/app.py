61eb1e4fe29c505d5681d4a2cbd85d07227087c9
Shaker Sharabi <shakersharabi100@gmail.com>
2025-02-06 15:29:46 +0200

    Add app.py file

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 def root():
     """root route
     """
-    return jsonify({'message', 'Bienvenue'})
+    return jsonify({'message': 'Bienvenue'}), 200
 
 
 if __name__ == "__main__":
