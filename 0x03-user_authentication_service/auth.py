ed6cdd30f1991eea33c6b873ff9080e8a9ad8453
Shaker Sharabi <shakersharabi100@gmail.com>
2025-02-06 17:15:13 +0200

    Add auth.py file

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -69,3 +69,14 @@ class Auth:
             return session_id
         except Exception:
             pass
+
+    def get_user_from_session_id(session_id: str) -> User:
+        """gets the user
+        """
+        if not session_id:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
