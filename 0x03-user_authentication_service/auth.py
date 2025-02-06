d8d7012ac0dc99cb0af585feee26b8142e39cef0
Shaker Sharabi <shakersharabi100@gmail.com>
2025-02-06 15:18:55 +0200

    Add auth.py file

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,6 +2,9 @@
 """Module that handles the authentication
 """
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
@@ -13,3 +16,23 @@ def _hash_password(password: str) -> bytes:
     hash_pwd = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
     return hash_pwd
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """registers the user
+        """
+        db = self._db
+        try:
+            user = db.find_user_by(email=email)
+            raise ValueError('User {} already exists'.format(email))
+        except NoResultFound:
+            hash_pwd = _hash_password(password)
+            user = db.add_user(email, hash_pwd)
+            return user
